5122bc8e6e8166e9b5ca6d5436fb6663b3f67634
Pashtet <pavelmatusyak@gmail.com>
2015-08-30 19:16:34 +0300

    blog context

---
+++ src/ScalabelSimpleBlog/Data/BlogContext.cs
@@ -0,0 +1,16 @@
+﻿using ScalabelSimpleBlog.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ScalabelSimpleBlog.Data
+{
+    public class BlogContext : DbContext
+    {
+        public IDbSet<Article> Articles { get; set; }
+
+        public IDbSet<Tag> Tags { get; set; }
+    }
+}
---
+++ src/ScalabelSimpleBlog/Entities/Article.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ScalabelSimpleBlog.Entities
+{
+    public class Article
+    { 
+        public int Id { get; set; }
+
+        [Column(TypeName = "varchar(MAX)")]
+        public string Body { get; set; }
+
+        public string TeaserText { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime EditedDate { get; set; }
+
+        public bool IsPublished { get; set; }
+
+        public ICollection<Tag> Tags { get; set; }
+    }
+}
