916e0acbf111fb97ccf5a99957001c8683a6d607
PavelMats <260188pavelmatusyak@gmail.com>
2015-09-01 21:31:25 +0300

    fixing

--- src/Business/ScalabelSimpleBlog.Business/Services/BlogCommandService.cs
+++ src/Business/ScalabelSimpleBlog.Business/Services/BlogCommandService.cs
@@ -7,9 +7,6 @@ using ScalabelSimpleBlog.Data.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ScalabelSimpleBlog.Entities;
 
 namespace ScalabelSimpleBlog.Business.Services
 {
