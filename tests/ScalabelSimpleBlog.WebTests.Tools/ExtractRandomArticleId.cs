5165e93a42e3ea26c21a896d0f4d7e92b13ec60b
Pashtet <pavelmatusyak@gmail.com>
2015-09-13 11:49:59 +0300

    test

--- tests/ScalabelSimpleBlog.WebTests.Tools/ExtractRandomArticleId.cs
+++ tests/ScalabelSimpleBlog.WebTests.Tools/ExtractRandomArticleId.cs
@@ -15,16 +15,54 @@ namespace ScalabelSimpleBlog.WebTests.Tools
             get { return "Extract Random Article Id"; }
         }
 
+        public override string RuleDescription
+        {
+            get { return "Extracts Random Article Id from blog page"; }
+        }
+
         public override void Extract(object sender, ExtractionEventArgs e)
         {
             if (e.Response.HtmlDocument != null)
             {
                 var document = e.Response.HtmlDocument;
-               // var articles = document.GetFilteredHtmlTags("a").Where(x => x);
+                // https://msdn.microsoft.com/en-us/library/ms243179.aspx  "show-article-link"
+                var articles = document.GetFilteredHtmlTags("a").Where(x => x.GetAttributeValueAsString("class").Contains("show-article-link")).ToList();
 
+                if(articles.Count > 0)
+                {
+                    var randomArticle = articles.Random();
+                    // data-article-id
+                    var articleId = randomArticle.GetAttributeValueAsString("data-article-id");
+                    e.WebTest.Context.Add(this.ContextParameterName, articleId);
+                    e.Success = true;
+                    return;
+                }
             }
             e.Success = false;
             e.Message = "Not Found";
+        }
+    }
+
+    public static class EnumerableHelper<E>
+    {
+        private static Random r;
+
+        static EnumerableHelper()
+        {
+            r = new Random();
+        }
+
+        public static T Random<T>(IEnumerable<T> input)
+        {
+            return input.ElementAt(r.Next(input.Count()));
+        }
+    }
+
+    public static class EnumerableExtensions
+    {
+        public static T Random<T>(this IEnumerable<T> input)
+        {
+            return EnumerableHelper<T>.Random(input);
         }
     }
 }
